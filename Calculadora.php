e50dd72edde8df8fd845afc04121bb06fbd0ebb6
Felipe Brito <139879477+FelipeBritoSP10@users.noreply.github.com>
2024-02-13 20:56:15 -0300

    Create Calculadora.php

---
+++ Calculadora.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <title>Tela de login</title>
+</head>
+<body>
+    <div class="bg-dark">
+        <div class="container">
+            <div class="card card-login mx-auto mt-5 col-8">
+                <div class="card card-header text-center">
+                    <strong>Calculadora</strong>
+                </div>
+                <div class="card-body">
+                    <form action="resultado.php" method="POST">
+                        <div class="form-group">
+                            <select name="opcao" class="form-control">
+                             <option selected>Escolha a operação</option>
+                             <option value="soma">Adição</option>
+                             <option value="sub">Subtração</option>
+                             <option value="mult">Multiplicação</option>
+                             <option value="div">Divisão</option>
+                            </select>
+                        </div>
+                        <br>
+                        <div class="form-group">
+                        <label for="">Digite um número</label>  
+                        <input type="number" class="form-control" name="num1" placeholder="Digite um número...">
+                        <br>  
+                        <label for="">Digite um número</label>  
+                        <input type="number" class="form-control" name="num2" placeholder="Digite um número...">
+                        </div>
+                        <br>
+                        <button class="btn btn-success">Calcular</button>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+    <center>
+    <h2>Desenvolvido por Felipe Brito</h2>
+    </center>
+</body>
+</html>
